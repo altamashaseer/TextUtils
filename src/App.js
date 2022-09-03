62524002ea74a556aa30373c9b39469c71c97a4f
Altamash <altamashaseer9@gmail.com>
2022-09-03 14:55:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+let name = 'altamash';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<></> this is jsx fragment to use multiple tags in one serve 
+    <>
+      <div className="container">
+        hello from react {name}
+      </div>
+    </>
   );
 }
 
